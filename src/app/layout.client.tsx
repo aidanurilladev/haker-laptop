4575fbc379cab71f96c0857e1f189e871ea90980
aidanurilladev <aidanurillaa@gmail.com>
2024-12-04 14:56:33 +0600

    first commit

---
+++ src/app/layout.client.tsx
@@ -0,0 +1,11 @@
+import { ReduxProvider } from "@/providers/ReduxProvider";
+import React, { FC, ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+const LayoutClient: FC<LayoutProps> = ({ children }) => {
+  return <ReduxProvider>{children}</ReduxProvider>;
+};
+
+export default LayoutClient;
