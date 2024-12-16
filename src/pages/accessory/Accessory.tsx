75e62cf811c3a8d50df5c41f550a3bc7564c53cf
aidanurilladev <aidanurillaa@gmail.com>
2024-12-16 19:20:44 +0600

    add pages

---
+++ src/pages/accessory/Accessory.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import scss from "./Accessory.module.scss"
+
+const Accessory = () => {
+  return (
+    <div>Accessory</div>
+  )
+}
+
+export default Accessory
---
+++ src/pages/catalog/Catalog.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import scss from "./Catalog.module.scss"
+
+const Catalog = () => {
+  return (
+    <div>Catalog</div>
+  )
+}
+
+export default Catalog
