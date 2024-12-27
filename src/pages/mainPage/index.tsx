a8e90b4ca908903d789a3297fe5bb1f6a911e4fc
맥북 <maegbug@maegbug-ui-MacBookAir.local>
2024-12-27 15:31:32 +0600

    detalnoud

--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import DetailNoud from '../detailNoud/DetailNoud';
   
 
 const MainPage = () => {
     return (
         <div>
-            
+            <DetailNoud/>
         </div>
     );
 };
