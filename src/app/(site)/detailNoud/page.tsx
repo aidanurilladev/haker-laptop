7c2487008a618a7440dee40b1e4c2b1de3a5ec37
맥북 <maegbug@maegbug-ui-MacBookAir.local>
2025-02-03 13:40:59 +0600

    ferisg

---
+++ src/app/(site)/detailNoud/page.tsx
@@ -0,0 +1,12 @@
+import DetailNoud from '@/pages/detailNoud/DetailNoud';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <DetailNoud/>
+        </div>
+    );
+};
+
+export default page;
